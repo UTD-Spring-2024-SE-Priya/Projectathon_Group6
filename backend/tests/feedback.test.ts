bbe81ced2ec79c1a035c8851d3e42a281d85a085
danielsuhcompsci <danielsuh112@yahoo.com>
2024-04-19 15:01:21 -0500

    tests for feedback

--- backend/tests/feedback.test.ts
+++ backend/tests/feedback.test.ts
@@ -1,3 +1,47 @@
-import { describe } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
+import { app } from "../src/app";
+import { CreateFeedbackInput } from "../src/models/feedback/types";
 
-describe
+const port = 5000;
+
+beforeAll(() => {
+    app.listen(port);
+    console.info(`Server started on port ${port}`);
+});
+
+describe("Feedback Tests", () => {
+    it("should create feedback for an idea", async () => {
+        const inputBody: CreateFeedbackInput = {
+            userId: 1,
+            ideaId: 1,
+            feedback: "This is a test feedback",
+        };
+
+        const response = await fetch(`http://localhost:${port}/feedback/create`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(inputBody),
+        });
+
+        expect(response.status).toBe(201);
+
+        const body = await response.json();
+        expect(body).toHaveProperty("id");
+        expect(body.userId).toBe(inputBody.userId);
+        expect(body.ideaId).toBe(inputBody.ideaId);
+        expect(body.feedback).toBe(inputBody.feedback);
+    });
+
+    it("should get all feedback", async () => {
+        const response = await fetch(`http://localhost:${port}/feedback`, {
+            method: "GET",
+        });
+
+        expect(response.status).toBe(200);
+
+        const body = await response.json();
+        expect(body).toBeInstanceOf(Array);
+    })
+})
