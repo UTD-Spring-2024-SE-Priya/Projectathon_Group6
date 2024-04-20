8390bda50545886dcfa6db0fbe0cda3c297d34d8
muaazabed <muaaz.abed@gmail.com>
2024-04-19 22:37:22 -0500

    connected idea generation endpoint to db

--- backend/src/models/projectIdea/controller.ts
+++ backend/src/models/projectIdea/controller.ts
@@ -4,6 +4,8 @@ import { Request, Response } from 'express';
 import { prisma } from "../../db";
 import { CreateIdeaInput } from "./types";
 
+import { ideaGeneration } from "../../utils"
+
 export class Controller {
     async getIdeasForUser(req: Request, res: Response) {
         const { userId } = req.params;
@@ -23,6 +25,43 @@ export class Controller {
     }
 
     async createProjectIdea(req: Request, res: Response) {
-        const { userId, description,  }: CreateIdeaInput = req.body;
+        const { userId, description, }: CreateIdeaInput = req.body;
+
+        if (!userId || !description) {
+            res.status(400).send('Missing required fields');
+            return;
+        }
+
+        const userInfo = await prisma.userInfo.findUnique({
+            where: {
+                userId: userId,
+            },
+        });
+
+        ideaGeneration(userInfo.skills, userInfo.programming_languages, userInfo.interests, 10).then((value) => {
+            this.rest(value, userId, description).then((idea) => {
+                res.status(201).json({
+                    id: idea.id,
+                    title: idea.title,
+                    description: idea.description,
+                    technologies: idea.technologies,
+                    userId: idea.userId,
+                });
+            });
+            
+        });
+    }
+
+    // part of createProjectIdea, result of callback hell lol
+    async rest(word: any, userId: any, description: string) {
+        return await prisma.projectIdea.create({
+            data: {
+                userId,
+                title: word,
+                description: description,
+                technologies: "lang here",
+            },
+        });
+    }
 
 }
--- backend/src/models/projectIdea/routes.ts
+++ backend/src/models/projectIdea/routes.ts
@@ -17,4 +17,15 @@ router.get('/:userId/all', async (req, res) => {
     }
 });
 
+// creates project idea
+
+router.post('/createidea', async (req, res) => {
+    try {
+        await controller.createProjectIdea(req, res);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send('Error in create project idea controller');
+    }
+});
+
 export default router;
